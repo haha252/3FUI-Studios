0cdc6a6050e6d0a700539d0f99952be50444d2ec
haha252 <2052249283@qq.com>
2026-01-08 23:01:00 +0800

    今天的AI有点蠢

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -452,13 +452,13 @@ const getDownloadLinks = (id) => {
                     <p class="whitespace-pre-wrap">{{ getDetail(item.id).basic?.description || '暂无描述' }}</p>
                   </div>
                   <div class="space-y-2">
-                    <div v-for="(link, idx) in getDownloadLinks(item.id)" :key="idx" class="flex items-center gap-2">
-                      <span class="text-white">下载:</span>
-                      <a :href="link.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
-                        {{ link.name || link.url }}
-                      </a>
-                    </div>
-                    <div v-if="getDetail(item.id).source?.url" class="flex items-center gap-2">
+                      <div v-for="(link, idx) in getDownloadLinks(item.id)" :key="idx" class="flex items-center gap-2">
+                        <span class="text-white">下载:</span>
+                        <a :href="link.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
+                          {{ link.url }}
+                        </a>
+                      </div>
+                      <div v-if="getDetail(item.id).source?.url" class="flex items-center gap-2">
                        <span class="text-white">来源:</span>
                        <a :href="getDetail(item.id).source.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
                         {{ getDetail(item.id).source.url }}
@@ -522,13 +522,13 @@ const getDownloadLinks = (id) => {
                     <p class="whitespace-pre-wrap">{{ getDetail(item.id).basic?.description || '暂无描述' }}</p>
                   </div>
                   <div class="space-y-2">
-                    <div v-for="(link, idx) in getDownloadLinks(item.id)" :key="idx" class="flex items-center gap-2">
-                      <span class="text-white">下载:</span>
-                      <a :href="link.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
-                        {{ link.name || link.url }}
-                      </a>
-                    </div>
-                    <div v-if="getDetail(item.id).source?.url" class="flex items-center gap-2">
+                      <div v-for="(link, idx) in getDownloadLinks(item.id)" :key="idx" class="flex items-center gap-2">
+                        <span class="text-white">下载:</span>
+                        <a :href="link.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
+                          {{ link.url }}
+                        </a>
+                      </div>
+                      <div v-if="getDetail(item.id).source?.url" class="flex items-center gap-2">
                        <span class="text-white">来源:</span>
                        <a :href="getDetail(item.id).source.url" target="_blank" class="text-blue-400 hover:text-blue-300 hover:underline break-all">
                         {{ getDetail(item.id).source.url }}
